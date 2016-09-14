fd8227db6761e791be0f8776461c63b44021a464
JimmyLee05 <NanJun05@163.com>
2016-09-14 11:01:10 +0800

    modify some detail

--- main/views.py
+++ main/views.py
@@ -9,8 +9,7 @@ from . forms import EditProfileForm, EditProfileAdminForm, PostForm,\
  from ..models import Permission, Role, User, Post, Comment
  from ..decorators import admin_required, permission_required
 
- __author__ = NanJun
-
+ __author__ = 'NanJun'
 
 @admin.after_app_request
 def after_request(response):
@@ -175,7 +174,7 @@ def follow(username):
 @login_required
 @permission_required(Permission.FOLLOW)
 def unfollow(username):
-	user = User.query.filter_by('.index').first()
+	user = User.query.filter_by('username=username').first()
 	if user is None:
 		flash('Invalid user.')
 		return redirect(url_for('.index'))
@@ -188,52 +187,84 @@ def unfollow(username):
 
 
 @main.route('/followers/<username>')
-def followed(username):
+def followers(username):
 	user = User.query.filter_by(username=username).first()
 	if user is None:
 		flash('Invalid user.')
 		return redirect(url_for('.index'))
 	page = request.args.get('page', 1, type=int)
 	pagination = user.followers.paginate(
-		page, per_page=current_user.config['FLASK_FOLLOWERS_PER_PAGE'],
+		page, per_page=current_app.config['FLASKY_FOLLOWERS_PER_PAGE'],
 		error_out=False)
-	
+	follows = [{'user': item.follower, 'timestamp': item.timestamp}
+				for item in pagination.items]
+	return render_template('followers.html', user=user, title="Followers of",
+							endpoint='.followers', pagination=pagination,
+							follows=follows)
+
+@main.route('/followed-by/<username>')
+def followed_by(username):
+	user = User.query.filter_by(username=username).first()
+	if user is None:
+		flash('Invalid user.')
+		return redirect(url_for('.index'))
+	page = request.args.get('page', 1, type=int)
+	pagination = user.followed.paginate(
+		page, per_page=current_app.config['FLASKY_FOLLOWERS_PER_PAGE'],
+		error_out=False)
+	follows = [{'user': item.followed, 'timestamp': item.timestamp}
+				for item in pagination.items]
+	return render_template('followers.html', user=user, title="Followed by",
+							endpoint='.followed_by', pagination=pagination,
+							follows=follows)
 
 
+@main.route('/all')
+@login_required
+def show_all():
+	resp = make_response(redirect(url_for('.index')))
+	resp.set_cookie('show_followed', '', max_age=30*24*60*60)
+	return resp
 
 
+@main.route('/followed')
+@login_required
+def show_followed():
+	resp = make_response(redirect(url_for('.index')))
+	resp.set_cookie('show_followed', '1', max_age=30*24*60*60)
+	return resp
 
 
+@main.route('/moderate')
+@login_required
+@permission_required(Permission.MODERATE_COMMENTS)
+def moderate():
+	page = request.args.get('page', 1, type=int)
+	pagination = Comment.query.order_by(Comment.timestamp.desc()).paginate(
+		page, per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'],
+		error_out=False)
+	comments = pagination.items
+	return render_template('moderate.html', comments=comments,
+							pagination=pagination, page=page)
 
 
+@main.route('/moderate/enable/<int:id>')
+@login_required
+@permission_required(Permission.MODERATE_COMMENTS)
+def moderate_enable(id):
+	comment = Comment.query.get_or_404(id)
+	comment.disable = False
+	db.session.add(comment)
+	return redirect(url_for('.moderate',
+							page=request.args.get('page', 1, type=int)))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@main.route('/moderate/disable/<int:id>')
+@login_required
+@permission_required(Permission.MODERATE_COMMENTS)
+def moderate_disable(id):
+	comment = Comment.query.get_or_404(id)
+	comment.disable = True
+	db.session.add(comment)
+	return redirect(url_for('.moderate',
+							page=request.args.get('page', 1, type=int)))
