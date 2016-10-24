530ea437a4557bf47f1720805cfa06891b0b3ace
JimmyLee05 <NanJun05@163.com>
2016-10-24 14:15:37 +0800

    modify file

--- PythonTraining/20161024.py
+++ PythonTraining/20161024.py
@@ -1,4 +1,6 @@
 import socket
+import os
+import sys
 
 def retBanner(ip, port):
 
@@ -12,24 +14,41 @@ def retBanner(ip, port):
 	except:
 		return
 
-def checkVulns(banner):
+def checkVulns(banner, filename):
 
-	f = open("vuln_banners.txt",'r')
+	f = open(filename, 'r')
 	for line in f.readlines():
 		if line.strip('\n') in banner:
-			print "[+] Server is vulnerable: "+banner.strip('\n')
+			print '[+] Server is vulnerable: ' +\
+				banner.strip('\n')
 
 def main():
 
-	portList = [21,22,25,80,110,443]
+	if len(sys.srgv) ==2:
+		filename = sys.argv[1]
+		if not os.path.isfile(filename):
+			print '[-]' + filename +\
+			' does not exist.'
+			exit(0)
+		if not os.access(filename, os.R_OK):
+			print '[-]' + filename +\
+				'access denied'
+			exit(0)
+	else:
 
-	for x in range(1, 255):
-		ip = '192.168.95.' + str(x)
-		for port in portList:
-			banner = retBanner(ip, port)
-			if banner:
-				print '[+]' + ip + ':' + banner
-				checkVulns(banner)
+		print '[-] Usage: ' + str(sys.argv[0]) +\
+		' <vuln filename> '
+		exit(0)
+		portList = [21,22,25,80,110,443]
+		for x in range(147, 150):
+			ip = '192.168.95.' + str(x)
+			for port in portList:
+				banner = retBanner(ip, port)
+				if banner:
+					print '[+]' + ip + ': ' + banner
+					checkVulns(banner, filename)
 
-if __name__ == '__main__'
+
+if __name__ == '__main__':
 	main()
+
