619b418a98a71082ae969427f6b1cf076359053b
JimmyLee05 <NanJun05@163.com>
2016-10-25 16:48:04 +0800

    modify

--- PythonTraining/UNIXPasswordCracking.py
+++ PythonTraining/UNIXPasswordCracking.py
@@ -1,17 +1,44 @@
 import zipfile
+import optparse
+from threading import Thread
 
-zFile = zipfile.ZipFile('evil.zip')
-passFile = open('dictionary.txt')
-
-for line in passFile.readlines():
-
-	password = line.strip('\n')
+def extractFile(zFile, password):
 
 	try:
-
 		zFile.extractall(pwd=password)
-		print '[+] Password = ' +password + '\n'
-		exit(0)
+		print '[+] Found password ' + password + '\n'
 
-	except Exception, e:
+	except:
 		pass
+
+def main():
+
+	parser = optparse.OptionParser("usage%prog "+\
+	"-f <zipfile> -d <dictionary>")
+
+	parser.add_option('-f', dest='zname', type='string',\
+	help='specify zip file')
+
+	parse.add_option('-d', dest='dname', type='string',\
+	help='specify dictionary file')
+	 (options, args) = parser.parse_args()
+
+	if (options.zname == None) | (option.dname == None):
+	 	print parser.usage
+	 	exit(0)
+
+	else:
+	 	zname = options.zname
+	 	dname = options.dname
+
+	zFile = zipfile.ZipFile(zName)
+	passFile = open(dName)
+
+	for line in passFile.readlines():
+
+		password = line.strip('\n')
+		t = Thread(target=extractFile, args=(zFile, password))
+		t.start()
+
+if __name__ == '__main__':
+	main()
