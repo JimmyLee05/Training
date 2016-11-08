01586497a40527f0a57d25ea3e27b22f02c6828f
NanJunLee <NanJun05@163.com>
2016-11-08 19:28:35 +0800

    add new file

--- PythonTraining/AnalysisSitus.py
+++ PythonTraining/AnalysisSitus.py
@@ -1,19 +1,69 @@
+import dpkt
+import socket
 import pygeoip
+import optparse
 
-gi = pygeopy.GeoIP('/opt/GeoIP/Geo.dat')
+gi = pygeoip.GeoIP('/opt/GeoIP/Geo.dat')
 
-def prientRecord(tgt):
+def retGeoStr(ip):
 
-	rec = gi.record_by_name(tgt)
-	city = rec['city']
-	region = rec['region_name']
-	country = rec['country_name']
-	long = rec['longitude']
-	lat = rec['latitude']
-	print '[*] Target: ' + tgt + 'Geo-located'
-	print '[+] '+str(city)+', '+str(region)+', '+str(country)
-	print '[+] Latitude: '+str(lat)+ ', Longitude: '+ str(long)
+	try:
+		rec = gi.record_by_name(ip)
+		city = rec['city']
+		country = rec['country_code3']
 
-tgt = '173.255.226.98'
-prientRecord(tgt)
+		if city != '':
+			geoLoc = city +', ' +country
+
+		else:
+			geoLoc = country
+
+		return geoLoc
+
+	except Exception, e:
+		return 'unregistered'
+
+def printPcap(pcap):
+
+	for (ts, buf) in pcap:
+		try:
+			eth = dpkt.ethernet.Ethernet(buf)
+			ip = eth.data
+			src = socket.inet_ntoa(ip.src)
+			dst = socket.inet_ntoa(ip.dst)
+			print '[+] Src: ' +src +' --> Dst: ' +dst
+			print '[+] Src: ' +retGeoStr(src) +' --> Dst: ' \
+				+retGeoStr(dest)
+		except:
+			pass
+
+def main():
+
+	parser = optparse.OptionParser('usage%prog -p <pcap file>')
+	parser.add_option('-p', dest='pacpFile', type='string',\
+		help='specify pcap filename')
+	(options, args) = parser.parse_args()
+
+	if options.pcapFile == None:
+		print parser.usage
+		exit(0)
+	pcapFile = options.pacpFile
+	f = open(pcapFile)
+	pacp = dpkt.pcap.Reader(f)
+	printPcap(pcap)
+
+if __name__ == '__main__':
+	main()
+
+
+
+
+
+
+
+
+
+
+
+
 
