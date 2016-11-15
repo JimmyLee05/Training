915bf02da3e1ac87637916eaedd24db6b37b1a4a
NanJunLee <NanJun05@163.com>
2016-11-15 17:40:17 +0800

    add C# test

---
+++ PythonTraining/TestProject/TestProject/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WelcomeYou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("use continue to quit circular：");
+            for (int i=0; i<5; i++)
+            {
+                if (i==2)
+                {
+                    continue;
+                }
+
+                Console.WriteLine("now i is：{0}", i);
+            }
+
+            Console.WriteLine("use break to quit");
+            for (int i=0; i<5; i++)
+            {
+                if (i==2)
+                {
+                    break;
+                }
+
+                Console.WriteLine("now the i is：{0}", i);
+            }
+
+            Console.Read();
+        }
+    }
+}
+
+
