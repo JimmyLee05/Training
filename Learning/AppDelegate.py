a164b722b669aa349c983c3c5199449c442ad6e9
JimmyLee05 <NanJun05@163.com>
2017-06-08 19:09:13 +0800

    modify

--- Learning/AppDelegate.py
+++ Learning/AppDelegate.py
@@ -58,10 +58,56 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		return false
 	}
 
+	func applicationWillEnterForeground(_ application: UIApplication) {
+		//恢复到前台 通知界面重新加载所有view
+		STJPushKit.shared().removeLocalNotification(nil)
+	}
+
+	func applicationDidBecomeActive(_ application: UIApplication) {
+		UIApplication.shared.applicationIconBadgeNumber = 0
+	}
+
+	func applicationWillTerminate(_ application: UIApplication) {
+		UIApplication.runningWhenBoot()
+		MYNTKit.shared.mynts.forEach { (mynt) in
+			if mynt.bluetoothState == .connected {
+				mynt.update { mynt.lastDisconnectTime = Int(Date().timeIntervalSince1970) }
+			}
+		}
+	}
+
+	func application(_ application: UIApplication,
+					 performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+		completionHandler(.newDate)
+	}
+
+	func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+		let result = UMSocialManager.default().handleOpen(url)
+		if !result {
+			// 其它如支付等SDK回调
+		}
+		return result
+	}
+
+	func application(_ application: UIApplication,
+					 didReceive notification: UILocalNotification) {
+		_ = STJPushKit.shared().didReceiveLocalNotification(notification.userInfo)
+	}
+
+	func application(_ application: UIApplication,
+					 didRegisterForTRemoteNotifacationsWithDeciceToken deviceToken: Data) {
+		STJPushKit.shared().registertoken(deviceToken)
+	}
 	
+	func application(_ application: UIApplication,
+					 didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
+		_ = STJPushKit.shard().didReceiveAPNS(userInfo)
+	}
 
-	)
-
-
+	func application(_ application: UIApplication,
+					 didReceiveRemoteNotification userInfo: [AnyHashable : Any],
+					 fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+		completionHandler(STJPushKit.shared().didReceiveAPNS(userInfo))
+	}
 
 }
