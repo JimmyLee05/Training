d8dfe36fff62cac7d32e8ac84acc5ec8fb855806
JimmyLee05 <NanJun05@163.com>
2017-06-12 16:24:56 +0800

    Supplement is complete

--- Learning/DebugViewController.py
+++ Learning/DebugViewController.py
@@ -75,3 +75,87 @@ class DebugViewController: BaseViewController {
 
 }
 
+extension DebugViewController: UITableViewDelegate, UITableViewDataSource, UIAlertViewDelegate {
+	
+	func tableView(_ tableView: UItableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+		if cell ==nil {
+			cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+			cell?.backgroundColor = UIColor.clear
+			cell?.selectionStyle = .none
+			cell?.textLabel?.font = UIFont.systemFont(ofSize: 16)
+			cell?.textLabel?.textColor = UIColor.white
+		}
+		cell?.textLabel?.text = types[indexPath.row].name
+		return cell!
+	}
+
+	func tableView(_ tableView: UItableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+		return 60
+	}
+
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		let type = types[indexPath.row]
+		switch type {
+		case .superdebug:
+			//弹出对话框
+			let alertView = UIAlertView(title: "请输入密钥"，
+										message: "",
+										delegate: self,
+										cancelButtonTitle: "取消"
+										otherButtonTitles: "确定")
+			alertView.alertViewStyle = .plainTextInput
+			alertView.tag = type.rawValue
+			alertView.show()
+		case .testLeanCloud:
+			let myntlog = AVObject(className: "mynt_log")
+			let formatter = DateFormatter()
+			formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+			myntlog.setObject("test", forKey: "log")
+			myntlog.setObject("test", forkey: "sn")
+			myntlog.setObject(formatter.string(from: Date()), forKey: "time")
+			myntlog.setObject(MYNTKit.shared.user?.alias, forKey: "alias")
+			myntlog.setObject(MYNTKit.shared.user?.userName, forKey: "userName")
+			myntlog.saveInBackground({ (succeeded. error) in
+				MTToast.show(succeed ? "successed": "failed \(error)")
+			})
+		case .webserver:
+			if WebServer.shared.isOpen {
+				Toast.show(message: "webserver已关闭，中间如果出现crash，请重启app")
+				WebServer.shared.close()
+			} else {
+				Toast.show(message: "webserver已启动, 中间如果出现crash， 请重启app")
+				WebServer.shared.open { _ in
+					tableView.reloadData()
+				}
+			}
+			tableView.reloadData()
+		default:
+			let viewController = type.viewController
+			viewController.title = type.name
+			removeBackBarButtonTitle()
+			push(viewController: viewController)
+
+		}
+	}
+
+	func alertView(_ alertView: UIAlertView, clickedButtonAt buttonIndex: int) {
+		switch alertView.tag {
+		case DebugType.superdebug.rawValue:
+			if buttonIndex == 1 {
+				let textField = alertView.textField(at: 0)
+				if let email = MYNTKit.shared.user?.email.lowercrased() {
+					if textField?.text == (email + "--mynt--slightech").md5 {
+						AppConfig.isSuperDebugMode = true
+						MTToast.show("开启完成")
+						tableView.reloadData()
+						return
+					}
+				}
+				MTToast.show("密钥错误")
+			}
+		default:
+			return
+		}
+	}
+}
