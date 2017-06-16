814343b5fe01138ae28b0f96300e0cf93bb4ae89
JimmyLee05 <NanJun05@163.com>
2017-06-16 13:54:56 +0800

    add new files

---
+++ JianSan/api.py
@@ -0,0 +1,33 @@
+import Foundation
+
+// api根路径 http://jiansan.6ag.cn
+let BASE_URL = "http://jiansan.6ag.cn"
+
+//提交
+let SUBMIT_FEEDBACK = "\(BASE_URL)/api/feedback"
+
+//获取分类列表
+let GET_CATEGORIES = "\(BASE_URL)/api/categories"
+
+//检测壁纸保存状态
+let CHECK_SAVE_STATUS = "\(BASE_URL)/api/status"
+
+/**
+根据分类id获取壁纸列表
+
+- parameter category_id: 分类id
+- returns: 返回api接口
+*／
+func GET_WALLPAPERS(_ category_id: Int) -> String {
+	return "\(BASE_URL)/api/wallpapers/\(category_id)";
+}
+
+/**
+根据壁纸id获取壁纸信息
+
+- parameter id: 壁纸id
+- returns: 返回api接口
+*/
+func GET_SHOW_WALLPAPER(_ id: Int) -> String {
+	return "\(BASE_URL)/api/show/\(id)"
+}
