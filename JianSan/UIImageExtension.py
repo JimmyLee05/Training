6bb915e5492d4baa5dd3081e37c67b8fac9516b4
JimmyLee05 <NanJun05@163.com>
2017-06-20 15:18:01 +0800

    add new file

---
+++ JianSan/UIImageExtension.py
@@ -0,0 +1,71 @@
+import UIKit
+
+extension UIImage {
+	
+	// 重新绘制图片
+	// - Parameters:
+	// - image: 原图
+	// - size: 绘制尺寸
+	// - Returns: 新图
+
+	func redrawImage(size: CGSize?) -> UIImage? {
+
+		//绘制区域
+		let rect = CGRect(origin: CGPoint(), size: size ?? CGSize.zero)
+
+		//开启图形上下文 size：绘图的尺寸 opaque: 透明度 scale: 屏幕分辨率系数，选0是当前设备的屏幕分辨率系数
+		UIGraphicsBeginImageContextWithOptions(rect.size, true, 0)
+
+		//绘制，在指定区域拉伸并绘制
+		draw(in: rect)
+
+		//从图形上下文获取图片
+		let result = UIGraphicsGetImageFromCurrentImageContext()
+
+		//关闭上下文
+		UIGraphicsEndImageContext()
+
+		return result
+	}
+
+	// 重新绘制圆形图片
+	//
+	// - Parameters:
+	// - image: 原图
+	// - size: 绘制尺码
+	// - bgColor: 裁剪区域外的背景颜色
+	// - Returns: 新图
+
+	func redrawOvalImage(size: CGSize?, bgColor: UIColor?) -> UIImage? {
+
+		//绘制区域
+		let rect = CGReat(origin: CGPoint(), size: size ?? CGSize.zero)
+
+		//开启图形上下文 size:绘图的尺寸 opaque:不透明 scale:屏幕分辨率系数，0会选择当前设备的屏幕分辨率系数
+		UIGraphicsBeginImageContextWithOptions(rect.size, true, 0)
+
+		//背景颜色填充
+		bgColor?.setFill()
+		UIRectFill(rect)
+
+		//圆形路径
+		let path = UIBezierPath(ovalIn: rect)
+
+		//进行路径裁切,后续的绘图都会出现在这个圆形区域内部
+		path.addClip()
+
+		//绘制图像，在指定区域拉伸并绘制
+		draw(in: rect)
+
+		//从图形上下文回去图片
+		let result = UIGraphicsGetImageFromCurrentImageContext()
+
+		//关闭上下文
+		UIGraphicsEndImageContext()
+
+		return result
+	}
+}
+
+
+
