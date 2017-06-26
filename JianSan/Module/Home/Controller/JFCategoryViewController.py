a40d28377ac6123bebbb64a0ec726a7d76bbf1e8
JimmyLee05 <NanJun05@163.com>
2017-06-26 19:16:56 +0800

    add new file

---
+++ JianSan/Module/Home/Controller/JFCategoryViewController.py
@@ -0,0 +1,153 @@
+import UIKit
+import SnapKit
+
+protocol JFCategoryViewControllerDelegate {
+	func didTappedPopularButton()
+	func didTappedBestButton()
+}
+
+class JFCategoryViewController: UIViewController {
+	
+	let categoryIdentifier = "categoryCell"
+
+	var categoriesArray = [JFCategoryModel]()
+
+	var delegate: JFCategoryViewControllerDelegate?
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		prepareUI()
+		loadCategoryData()
+	}
+
+	/**
+	准备视图
+	*/
+	fileprivate func prepareUI() {
+
+		view.backgroundColor = UIColor.white
+		view.addSubview(collectionView)
+		view.addSubview(popularButton)
+		view.addSubview(bestButton)
+
+		popularButton.snp.makeConstraints { (make) in
+			make.left.right.equalTo(0)
+			make.top.equalTo(collectionView.snp.buttom).offset(1.5)
+			make.height.equalTo((SCREEN_HEIGHT - 64 - ((SCREEN_HEIGHT - 64) / 4.6)) /2)
+		}
+
+		bestButton.snp.makeConstraints { (make) in
+			make.left.right.equalTo(0)
+			make.top.equalTo(popularButton.snp.bottom).offset(1.5)
+			make.height.equalTo(popularButton)
+		}
+	}
+
+	/**
+	加载分类数据
+	*/
+	@objc fileprivate func loadCategoryData() {
+		JFCategoryModel.loadCategoriesFromNetwork { (categoriesArray, error) in
+			guard let categoriesArray = categoriesArray, error == nil else {
+				return
+			}
+
+			self.categoriesArray = categoriesArray
+			self.collectionView.reloadData()
+		}
+	}
+
+	/**
+	点击了热门
+	*/
+	@objc fileprivate func didTappedPopularButton(_ button: UIButton) {
+		delegate?.didTappedPopularButton()
+	}
+
+	/**
+	点击了最佳锁屏
+	*/
+	@objc fileprivate func didTappedBestButton(_ button: UIButton) {
+		delegate?.didTappedBestButton()
+	}
+
+	//MARK: -懒加载
+	// collectionView
+	fileprivate lazy var collectionView: UICollectionView = {
+
+		let layout = UICollectionViewFlowLayout()
+		layout.minmumInteritemSpacing = 1.5
+		layout.mimimumLineSpacing = 1.5
+		layout.scrollDirection = .herizontal
+		layout.itemSize = CGSize(width: (SCREEN_WIDTH - 4.5) / 4, height: (SCREEN_HEIGHT - 64) / 4.6)
+
+		let collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: SCREEN_WIDTH,
+			height: (SCREEN_HEIGHT - 64) / 4.6), collectionViewLayout: layout)
+		collectionView.backgroundColor = UIColor.white
+		collectionView.bounces = false
+		collectionView.dataSource = self
+		collectionView.delegate = self
+		collectionView.showsHorizontalScrollIndicator = false
+		collectionView.register(UINib(nibName: "JFCategoryCell", bundle: nil),
+			forCellWithReuseIdentifier: self.categoryIdentifier)
+		return collectionView
+	}()
+
+	//热门
+	fileprivate lazy var popularButton: UIButton = {
+		let popularButton = UIButon(type: .custom)
+		popularButton.setBackgroundImage(UIImage(named: "category_popular"), for:
+			UIControlState())
+		popularButton.addTarget(self, action: #selector(didTappedPopularButton(_:)), for:
+			UIControlEvents.touchUpInside)
+		return popularButton
+	}()
+
+	//最佳锁屏
+	fileprivate lazy var bestButton: UIButton = {
+
+		let bestButton = UIButton(type: .custom)
+		bestButton.setBackgroundImage(UIImage(named: "category_best"), for: UIControlState())
+		bestButton.addTarget(self, action: #selector(didTappedBestButton(_:)), for:
+			UIControlEvents.touchUpInside)
+		return bestButton
+	}()
+
+}
+
+//Mark: -UICollectionViewDataSource, UICollectionViewDelegate
+extension JFCategoryViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+	
+	func collectionView(_ collection: UICollectionView, numberOfItemInSection section: Int)
+		-> Int {
+		return categoriesArray.count
+	}
+
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: indexPath) 
+		-> UICollectionViewCell {
+		let item = collectionView.dequeueReusableCell(withReuseIdentifier: categoryIdentifier,
+			for: indexPath) as! JFCategoryCell
+		item.model = categoriesArray(indexPath.item)
+		return item
+	}
+
+	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath)
+		{
+		let popularVc = JFPopularViewController()
+		popularVc.category_id = categoriesArray[indexPath.item].id
+		popularVc.category_title = categoriesArray[indexPath.item].name ?? ""
+		navigationController?.pushViewController(popularVc, animated: true)
+		}
+}
+
+
+
+
+
+
+
+
+
+
+
