0c2f2891dbd12bb573e0e85cd48603acf98ae076
JimmyLee05 <NanJun05@163.com>
2017-07-12 09:36:33 +0800

    delete

--- Learning/Swift/Vendors/SDK/IM-SDK/MessagePlugin/InputViewPluginImagePickerh.py
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <WXOUIModule/YWUIFMWK.h>
-#import <WXOpenIMSDKFMWK/YWFMWK.h>
-
-@interface InputViewPluginImagePicker : NSObject<YWInputViewPluginProtocol>
-
-@property (nonatomic, weak) YMMessageInputView *inputViewRef;
-
-@end
--- Learning/Swift/Vendors/SDK/IM-SDK/Widget/IMNoDataView.py
+++
@@ -1,5 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface IMNoDataView : UIView
-
-@end
