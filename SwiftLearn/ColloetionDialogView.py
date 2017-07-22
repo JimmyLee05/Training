a1fe3039bafb786929ddeea9ff077b330db25871
JimmyLee05 <NanJun05@163.com>
2017-07-22 16:48:33 +0800

    delete

--- SwiftLearn/ColloetionDialogView.py
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-typealias DialogSelectedHandler = (_ dialog: BaseDialog?, _ mynt: Mynt) -> Void
-
-class CollectionDialogView: DialogBaseView {
-	
-	@IBOutlet weak var neverTopConstraint: NSLayoutConstraint!
-	@
-}
