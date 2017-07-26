eb2d5585b6834918e4fb20a9e9b32a03e437c363
JimmyLee05 <NanJun05@163.com>
2017-07-26 16:09:24 +0800

    add new files

--- SwiftLearn/CalendarView.py
+++ SwiftLearn/CalendarView.py
@@ -110,39 +110,74 @@ public class CalendarView: UIView, UICollectionViewDataSource, UICollectionViewD
 			}
 		}
 
-		
+		layout = UICollectionViewFlowLayout()
+		layout.scrollDirection = .herizontal
+		collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+		collectionView?.backgroundColor = .clear
+		collectionView?.delegate = self
+		collectionView?.dataSource = self
+		collectionView?.isPagingEnabled = true
+		collectionView?.translatesAutoresizingMaskIntoConstraints = false
+		collectionView.showsVerticalScrollIndicator = false
+		collectionView.showsHorizontalScrollIndicator = false
+		collectionView.layer.masksToBounds = false
+		collectionView?.register(CalendarCollectionViewCell.self, forCellWithReuseIdentifier: "CalendarCollectionViewCell")
+		addSubview(collectionView!)
+		updateCollectionViewConstraints()
+	}
+
+	public override func layoutSubviews() {
+		super.layoutSubviews()
+
+		updateCollectionViewConstraints()
+		let width = (UIScreen.main.bounds.width - bounds.height) / 7
+		layout.itemSize = CGSize(width: width, height: bounds.height)
+		layout.minimumLineSpacing = 0.01
+		layout.minimumInteritemSpacing = 0.01
+		layout.sectionInset = UIEdgeInsetsMake(0, 0, 0, 0)
+
+		collectionView?.layoutIfNeeded()
+		collectionView?.setNeedsLayout()
+		collectionView?.setContentOffset(CGPoint(x: collectionView.contentSize.width - collectionView.frame.width, y: 0), animated: false)
+	}
+
+	fileprivate func updateCollectionViewConstraints() {
+		if collectionView == nil { return }
+		collectionView.removeConstraints(widthConstraints)
+		collectionView.removeConstraints(heightConstraints)
+
+		widthConstraints = NSLayoutConstraint.constraints(withVisualFormat: "H:|-(d)-[view]-(d)-|",
+														  options: .directionLeftToRight,
+														  metrics: ["d": bounds.height / 2],
+														  views: ["view": collectionView!])
+		heightConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[view]-0-|",
+														   options: .directionLeftToRight,
+														   metrics: ["d": bounds.height / 2],
+														   views: ["view": collectionView!])
+		addConstraints(widthConstraints)
+		addConstraints(heightConstraints)
+	}
+
+	public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return dates.count
+	}
+
+	public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CalendarCollectionViewCell", for: indexPath) as? CalendarCollectionViewCell
+		let date = dates[indexPath.row]
+		cell?.label?.text = "\(date.dayString)"
+		cell?.label?.backgroundColor = selectedDate == date ? UIColor(red:0.24, green:0.56, blue:1.00, alpha:1.00) : .clear
+		cell?.label?.textColor = selectedDate == date ? .white : date.timeIntervalSince1970 > todayDate.timeIntervalSince1970 ? UIColor(red:0.77, green:0.77, blue:0.77, alpha:1.00) : .black
+		return cell!
+	}
+
+	public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+		let date = dates[indexPath.row]
+		if date.timeIntervalSince1970 > todayDate.timeIntervalSince1970 { return }
+		selectedDate = date
+		collectionView.reloadDate()
+
+		delegate?.didSelectDay(calendarView: self, date: date)
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
