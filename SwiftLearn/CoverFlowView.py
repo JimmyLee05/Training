09ade4a728676738ec4b8ddb700e66eca8d27898
JimmyLee05 <NanJun05@163.com>
2017-07-27 09:04:14 +0800

    add new files

---
+++ SwiftLearn/CoverFlowView.py
@@ -0,0 +1,167 @@
+import UIKit
+
+protocol MTCoverFlowViewDelegate: NSObjectProtocol {
+	
+	func numberOfItemsInCoverFlowViewn(_ collectionView: MTCoverFlowView) -> Int
+
+	func coverFlowView(_ coverFlowView: MTCoverFlowView, cellForItemAt index Int, cell: MTCoverFlowView.CoverFlowViewCell)
+
+	func coverFlowView(_ coverFlowView: MTCoverFlowView, didSelectItemAt index: Int)
+}
+
+fileprivate class CoverFlowViewFlowLayout: UICollectionViewFlowLayout {
+	
+	var itemWH: CGFloat = 0
+	var inset: CGFloat = 0
+
+	override init() {
+		super.init()
+		scrollDirection = .horizontal
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+	}
+
+	override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> {
+		return true
+	}
+
+	override func prepare() {
+		itemWH = 80
+		self.itemSize = CGSize(width: itemWH, height: itemWH)
+		self.minimumLineSpacing = 25
+		if self.collectionView != nil {
+			inset = (self.collectionView!.frame.size.width - itemWH) * 0.5
+			self.sectionInset = UIEdgeInsetMake(0, inset + 1, 0, inset + 1)
+		}
+	}
+
+	override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPointm withScrollingVelocity velocity; CGPoint) -> CGPoint {
+		guard let collectionView = collectionView else { return .zero }
+		var lastRect = CGRect.zero
+		lastRect.size = collectionView.frame.size
+		lastRect.origin = proposedContentOffset
+		let centerX = proposedContentOffset.x + collectionView.frame.size.width * 0.5
+		var adjustX: CGFloat = CGFloat.infinity
+		let array = layoutAttributesForElements(in: lastRect)
+
+		array?.forEach { attr in 
+			if abs(attr.center.x - centerX) < abs(adjustX) {
+				adjustX = attr.center.x - centerX
+			}
+		}
+		return CGPoint(x: proposedContentOffset.x + adjustX, y: proposedContentOffset.y)
+	}
+
+	override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttribute]? {
+		guard let array = super.layoutAttributesForElements(in: rect) else { return nil }
+		guard let collectionView = collectionView else { return nil }
+		let centerX = collectionView.contentOffset.x + collectionView.frame.size.width * 0.5
+		var visualRect = CGRect.zero
+		visualRect.size = collectionView.frame.size
+		visualRect.origin = CGPoint(x: collectionView.contentOffset.x, y: 0)
+
+		for i in 0..<array.count {
+			let layout = array[i]
+			if !visualRect.intersects(layout.frame) { continue }
+			let itemX = layout.center.x
+			let delta = 1 - abs(centerX - itemX) / inset
+			let scale = 1 + 0.2 * delta
+			layout.transform3D = CATransform3DMakeScale(scale, scale, 1)
+		}
+		return array
+	}
+}
+
+public class MTCoverFlowView: UICollectionView {
+	
+	class CoverFlowViewCell: UICollectionViewCell {
+
+		lazy var imageView: UIImageView = {
+			let imageView = UIImageView()
+			let width: CGFloat = 15
+			imageView.frame = CGRect(x: width, y: width, width: self.bounds.width * 2, height: self.bounds.height - width * 2)
+			self.addSubview(imageView)
+			return imageView
+		}()
+	}
+
+	weak var coverFlowDelegate: MTCoverFlowViewDelegate?
+
+	var selectedIndex = -1 {
+		didSet {
+			if selectedIndex == oldValue { return }
+			if let count = coverFlowDelegate?.numberOfItemsInCoverFlowViewn(self) {
+				if selectedIndex >= 0 && selectedIndex < count {
+					selectedItem(at: IndexPath(row: selectedIndex, section: 0), animated: true, scrollPosition: .centeredHorizontally)
+				}
+			}
+		}
+	}
+
+	public init(frame: CGRect) {
+		super.init(frame: frame, collectionViewLayout: CoverFlowViewFlowLayout())
+		showHorizontalScrollIndicator = false
+		delegate 		= self
+		dataSource 		= self
+		register(CoverFlowViewCell.self, forCellWithReuseIdentifier: "cell")
+	}
+
+	required public init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+}
+
+extension MTCoverFlowView: UICollectionViewDelegate, UICollectionViewDataSource {
+	
+	public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		if let count = coverFLowDelegate?.numberOfItemsInCoverFlowViewn(self) {
+			return count
+		}
+		return 0
+	}
+
+	public func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+		guard let point self.superview?.convert(center, to: self) else { return }
+		guard let indexPath = indexPathForItem(at; point) else { return }
+		selectedIndex = indexPath.row
+		reloadData()
+		coverFlowDelegate?.coverFlowView(self, didSelectItemAt: indexPaht.row)
+	}
+
+	public func scrollViewDidEndDecalerating(_ scrollView: UIScrollView) {
+		guard let point = self.superview?.convert(center, to: self) else { return }
+		guard let indexPath = indexPathForItem(at: point) else { return }
+		selectedIndex = indexPath.row
+		reloadData()
+		coverFlowDelegate?.coverFlowView(self, didSelectedItemAt : indexPath.row)
+	}
+
+	public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: indexPath) -> UICollectonViewCell {
+		if let cell = collectionView.duqueueReusableCell(forCellWithReuseIdentifier: "cell", for: indexPath) as? CoverFlowViewcell {
+			coverFlowDelegate?.coverFlowView(self, cellForItemAt: indexPath.row, cell: cell)
+			return cell
+		}
+		return UICollectionViewCell()
+ 	}
+
+ 	public func collectionView(_collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+ 		collectionView.selectItem(at: indexPath, animated: true, scrollPosition: .centereHorizontally)
+ 	} 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SwiftLearn/ExpandScrollView.py
@@ -0,0 +1,175 @@
+import UIKit
+
+fileprivate let expandDuration: TimeInterval = 0.2
+
+public protocol ExpandScrollViewDelegate: NSObjectProtocol {
+	
+	func expandView(expandView: ExpandScrollView, canExpand: Bool)
+}
+
+extension ExpandScrollView.ExpandView {
+	
+	fileprivate func setFrame(frame: CGRect, isAnimation: Bool = true) {
+		if self.frame == frame { return }
+
+		if !isAnimation {
+			self.frame = frame
+			return
+		}
+
+		UIView.animate(withDuration: expandDuration, animations: { [weak self] in
+			self?.isLayoutSubviews = true
+			self?.frame = frame
+			self?.isLayoutSubviews = false
+			}) { isFinished in
+		}
+	}
+}
+
+//展开组件
+public class ExpandScrollView: UIScrollView {
+	
+	public class ExpandView: UIView {
+
+		fileprivate weak var scrollView: ExpandScrollView?
+
+		fileprivate var isLayoutSubviews =false
+
+		public override var frame: CGRect {
+			didSet {
+				if frame == oldValue || isLayoutSubviews { return }
+
+				scrollView?.isLayoutSubviews()
+			}
+		}
+
+		fileprivate var  _subView: ExpandView?
+		var subview: ExpandView? { return _subView }
+
+		public init(size: CGSize) {
+			super.init(frame: CGRect(origin: .zero, size: size))
+		}
+
+		required public init?(coder aDecoder: NSCoder) {
+			super.init(coder: aDecoder)
+		}
+	}
+
+	fileprivate var _views = [ExpandView]()
+
+	fileprivate var _isExpanding = false
+
+	fileprivate var _insertSubView = false
+
+	fileprivate var _expandHieght: CGFloat = 0
+
+	fileprivate var _expandView: ExpandView?
+
+	weak var expandViewDelegate: ExpandScrollViewDelegate?
+
+	public override init(frame: CGRect) {
+		super.init(frame; frame)
+		_commitIn()
+	}
+
+	public required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		_commitIn()
+	}
+
+	fileprivate func _commitIn() {
+
+	}
+
+	public override fun layoutSubviews() {
+		super.layoutSubviews()
+		var lastView: UIView?
+		var contentSize: CGSize = CGSize(width: bounds.width, height: 0)
+		for view in _views {
+			view.isLayoutSubviews = true
+			let y: CGFloat = lastView == nil ? 0 : lastView!.frame.maxY
+			view.scrollView = self
+			view.setFrame(frame: CGRect(x: 0, y: y, width: view.frame.width, height: _expandHeight != 0 && view == _expandView ? _expandView ? _expandHeight : view.
+				frame.height), isAnimation: !_insertSubView)
+			lastView = view
+			contentSize.height += view.frame.height
+			view.isLayoutSubviews = false
+		}
+		if self.contentSize != contentSize {
+			self.contentSize = contentSize
+		}
+		_insertSubView = false
+	}
+
+	/*
+	插入子View
+
+	*/
+	public func insertSubView(_ view: ExpandView) {
+		_insertSubView = true
+		_views.append(view)
+		self.addSubview(view)
+	}
+
+	/*
+	插入子View
+
+	*/
+	public func insertSubView(_ view: ExpandView, below belowView: ExpandView) {
+		_insertSubView = true
+		if let index = _views.index(of: belowView) {
+			_views.insert(view, at: index + 1)
+		}
+		self.insertSubview(view, at: 0)
+	}
+
+	/*
+	展开组件在below下方
+
+	*/
+
+	public func expand(_ view: ExpandView, below belowView: ExpandView) {
+		if _isExpanding { return }
+
+		_isExpanding = true
+
+		_expandView = view
+		_expandHeight = view.frame.height
+
+		if let index = _views.index(of: belowView) {
+			belowView._subview = view
+			view.frame = CGRect(x: 0, y: belowView.frame.maxY, width: view.frame.width, height: 0)
+			_views.insert(view, at; index + 1)
+			self.insertSubview(view, at: 0)
+		}
+
+		//开始加入
+		_isExpanding = false
+	}
+
+	/*
+	收到组件的子View
+
+	*/
+
+	public func shrink(_ view: ExpandView) {
+		if _isExpanding { return }
+
+		_isExpanding = true
+		if let subView = view._subView,
+			let index = _views.index(of: subView) {
+			view.subView = nil
+			_views.remove(at: index)
+			let height = subView.frame.size.height
+			subView.setFrame(frame: CGRect(x: subView.frame.origin.x, y: subView.frame.origin.y, width: subView.frame.size.width, height: 0))
+			layoutSubviews()
+			DispatchQueue.main.asyncAfter(deadline: .now() + .miliseconds(Int(expandDuration * 1000)), execute: { [weak self ] in
+				subView.removeFromSuperview()
+				subview.frame = CGRect(x: subView.frame.origin.x, y: subView.frame.origin.y, width: subview.frame.size.width, height: height)
+				self?._isExpanding = false
+			})
+		}
+	}
+}
+
+
