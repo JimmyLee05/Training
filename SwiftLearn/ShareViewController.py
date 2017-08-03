dedeb45d3369cbd1602f74c9e3f2499eb1973d39
JimmyLee05 <NanJun05@163.com>
2017-08-03 18:10:50 +0800

    add new files

--- SwiftLearn/ShareViewController.py
+++ SwiftLearn/ShareViewController.py
@@ -24,6 +24,55 @@ class ShareViewController: BaseViewController {
 		title = NSLocalizedString("SHARE_ASK_FRIEND", comment: "求助")
 		setLeftBarButtonItem(image: UIImage(named: "setting_add_safezone_close"))
 
-		
+		messageLabel.numberOfLines = 0
+		messageLabel.text = NSLocalizedString("SHARE_ASK_FRIEND_MESSAGE", comment: "求助")
+		titleLabel.text   = NSLocalizedString("SHARE_ASK_FRIEND", comment: "求助")
+		linkButton.setButtonBackgroundColorStyle(ColorStyle.kBlueGradientColor)
+		wechatButton.setButtonBackgroundColorStyle(ColorStyle.kGreenGradientColor)
+		linkButton.setTitle(NSLocalizedString("SHARE_APP_LINK", comment: "分享链接"), for: .normal)
+		wechatButton.setTitle(NSLocalizedString("SHARE_TO_WECHAT", comment: "分享微信"), for: .normal)
+	}
+
+	override func leftBarButtonClickedHandler() {
+		dismissNavigationController(animated: true)
+	}
+
+	@IBAction func didClickLinkButton(_ sender: Any) {
+		self.gotoAskPreviewViewController(.link)
+	}
+
+	@IBAction func didClickWechatButton(_ sender: Any) {
+		guard let mynt = sn?.mynt else { return }
+		if mynt.hardwareType == .CC25XX {
+			MTToast.show(NSLocalizedString("SHARE_FIRM_NOT_SUPPORT", comment: ""))
+			return
+		}
+		if Int(mynt.software)! <= 25 {
+			MTToast.show(NSLocalizedString("SHARE_PLEASE_UPDATE_FIRM", comment: ""))
+			return
+		}
+
+		mynt.authorizeWechat(success: {
+				self.gotoAskPreviewViewController(.wechat)
+		}) { msg in
+			MTToast.show(msg)
+		}
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+	}
+
+	/**
+	求助帮找
+
+	*/
+	func gotoAskPreviewViewController(_ type: SCShareType) {
+		let viewController 			= AskPreviewViewController()
+		viewController.sn 			= sn
+		viewController.shareType 	= type
+		self.present(BaseNavigationController(rootViewController: viewController), animated: true, completion: nil)
 	}
 }
+
+
