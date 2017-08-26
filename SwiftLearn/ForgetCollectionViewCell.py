462cde5a588051def7bdf0163635221cec687dab
JimmyLee05 <NanJun05@163.com>
2017-08-26 12:38:11 +0800

    add new files

---
+++ SwiftLearn/ForgetCollectionViewCell.py
@@ -0,0 +1,13 @@
+import UIKit
+
+class ForgetCollectionViewCell: UICollectionViewCell {
+		
+	@IBOutlet weak var imageView: UIImageView!
+
+	@IBOutlet weak var ownerView: UIView!
+
+	override func awakeFromNib() {
+		super.awakeFromNib()
+	}
+
+}
---
+++ SwiftLearn/LoginView.py
@@ -0,0 +1,76 @@
+import UIKit
+import MYNTKit
+
+class LoginView: UIView, ImageEmailTextFieldDelegate, ImageTextFieldDelegate {
+	
+	@IBOutlet weak var contentView: UIView!
+	@IBOutlet weak var triangleView: TriangleView!
+	@IBOutlet weak var emailTextField: ImageEmailTextField!
+	@IBOutlet weak var usernameTextField: ImageTextField!
+	@IBOutlet weak var passwordTextField: ImageTextField!
+	@IBOutlet weak var loginButton: GradientButton!
+	@IBOutlet weak var switchLoginButton: UIButton!
+	@IBOutlet weak var switchSignButton: UIButton!
+	@IBOutlet weak var switchForgetView: UIView!
+	@IBOutlet weak var forgetLabel: UILabel!
+
+	@IBOutlet weak var contentBootomConstrint: NSLayoutConstraint!
+
+	@IBOutlet weak var usernameHeightConstraint: NSLayoutConstraint!
+
+	@IBOutlet weak var usernameTopConstraint: NSLayoutConstraint!
+
+	@IBOutlet weak var triangleLeftConstraint: NSLayoutConstraint!
+
+	var loginHandler: ((_ isRegister: Bool) -> Void)?
+
+	let selectedTextColor 	= UIColor(red:0.41, green:0.41, blue:0.42, alpha:1.00)
+	let unselectedTextColor = UIColor(red:0.57, green:0.57, blue:0.58, alpha:1.00)
+
+	override func awakeFromNib() {
+		layer.masksToBounds = true
+		//初始化
+		usernameTopConstraint.constant 		= 0
+		usernameHeightConstraint.constant 	= 0
+
+		//设置箭头向下
+		triangleView.direction = .Bottom
+
+		//多语言
+		loginButton.setTitle(NSLocalizedString("LOGIN", comment: "登录"), for: .normal)
+		switchLoginButton.setTitle(NSLocalizedString("LOGIN", comment: "登录"), for: .normal)
+		switchSignButton.setTitle(NSLocalizedString("REGISTER", comment: "注册"), for: .normal)
+		forgetLabel.text = NSLocalizedString("FORGET_PASSWORD", comment: "忘记密码")
+		emailTextField.placeHolder = NSLocalizedString("EMAIL_ADDRESS", comment: "电子邮箱")
+		usernameTextField.placeHolder = NSLocalizedString("USER_NAME", comment: "用户名")
+		passwordTextField.placeHolder = NSLocalizedString("PASSWORD", comment: "密码")
+
+		emailTextField.keyboardType 			= .emailAddress
+		emailTextField.returnKeyType 			= .next
+		emailTextField.delegate 				= self
+		usernameTextField.returnKeyType 		= .next
+		usernameTextField.delegate 				= self
+		passwordTextField.returnKeyType 		= .done
+		passwordTextField.delegate 				= self
+	}
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
