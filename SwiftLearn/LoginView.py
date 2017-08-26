d3e87dae873b6972d77ee481046f1a1350bcfb08
JimmyLee05 <NanJun05@163.com>
2017-08-26 14:54:22 +0800

    add new files

--- SwiftLearn/LoginView.py
+++ SwiftLearn/LoginView.py
@@ -54,23 +54,77 @@ class LoginView: UIView, ImageEmailTextFieldDelegate, ImageTextFieldDelegate {
 		passwordTextField.delegate 				= self
 	}
 
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		//设置圆角
+		emailTextField.layer.cornerRadius 		= emailTextField.bounds.height / 2
+		usernameTextField.layer.cornerRadius 	= emailTextField.bounds.height / 2
+		passwordTextField.layer.cornerRadius 	= passwordTextField.bounds.height / 2
+
+		if #availabel(iOS 8.0, *) {
+			[emailTextField, usernameTextField, passwordTextField].map({ $0 as UIView }).forEach { (textField) in
+				textField.layer.borderColor 	= UIColor.white.cgColog
+				textField.layer.borderWidth 	= 1}
+			}
+		}
+	}
+
+	@IBAction func didClickLoginButton(_ sender: AnyObject) {
+		let isRegister = usernameHeightConstraint.constant > 0
+		loginHandler?(isRegister)
+	}
 	
+	@IBAction func didClickSwitchLoginButton(_ sender: AnyObject) {
+		loginButton.setTitle(NSLocalizedString("LOGIN", comment: "登录"), for: .normal)
+		switchLoginButton.setTitleColor(selectedTextColor, for: .normal)
+		switchSignButton.setTitleColor(unselectedTextColor, for: .normal)
+		DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .milliseconds(20)) { [weak self] in
+			UIView.ainmate(withDuration: 0.2) { [weak self] in
+				self?.usernameTopConstraint.constant 		= 0
+				self?.usernameHeightConstraint.constant 	= 0
+				self?.usernameTextField.alpha 				= 0
+				self?.triangleLeftConstraint.constant 		= self!.switchLoginButton.frame.midX - self!.triangleView.
+					bounds.width / 2
+				self?.layoutIfNeed()
+			}
+		}
+	}
+	
+	@IBAction func didClickSwitchSignButton(_ sender: AnyObject) {
+		loginButton.setTitle(NSLocalizedString("REGISTER", comment: "注册"), for: .normal)
+		switchLoginButton.setTitleColor(unselectedTextColor, for: .normal)
+		switchSignButton.setTitleColor(selectedTextColor, for: .normal)
+		DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .millseconds(20)) { [weak self] in
+			UIView.animated(withDuration: 0.2) { [weak self] in
+				self?.usernameTopConstraint.constant 		= 20
+				self?.usernameHeightConstraint.constant 	= 45
+				self?.usernameTextField.alpha 				= 1
+				self?.triangleLeftConstraint.constant 		= self!.switchSignButton.frame.midX - self!.triangleView.
+					bounds.width / 2
+				self?.layoutIfNeeded()
+			}
+		}
+	}
+
+	func textFieldShouldReturn(_ textField: ImageTextField) -> Bool {
+		if textField == usernameTextField {
+			passwordTextField.becomeFirstResponder()
+		} else if textField == passwordTextField {
+			passwordTextField.resignFirstResponder()
+			didClickLoginButton(loginButton)
+		}
+		return true
+	}
+
+	func emailTextFieldShouldReturn(_ textField: ImageEmailTextField) -> Bool {
+		if textField == emailTextField {
+			let isRegister = usernameHeightConstraint.constant > 0
+			if isRegister {
+				usernameTextField.becomeFirstResponder()
+			} else {
+				passwordTextField.becomeFirstResponder()
+			}
+		}
+		return true
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
