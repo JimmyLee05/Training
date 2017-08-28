c383f9e7f95f62dcbf1a75daab48492aa8a23d01
JimmyLee05 <NanJun05@163.com>
2017-08-28 10:29:39 +0800

    add new files

--- SwiftLearn/MTImageTableViewCell.py
+++ SwiftLearn/MTImageTableViewCell.py
@@ -1,2 +1,14 @@
 import UIKit
 
+class MTImageTableViewCell: MTBaseTableViewCell {
+	
+	@IBOutlet weak var nameLabel: UILabel!
+	@IBOutlet weak var headImageView: UIImageView!
+
+	var contentLayer: CALayer?
+
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		headImageView.layer.cornerRaiuds = headImageView.bounds.hieght / 2
+	}
+}
