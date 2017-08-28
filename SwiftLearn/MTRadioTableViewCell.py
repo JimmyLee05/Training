9a3f326f62e288c8da2cef6dcd648b8ec13af0e7
JimmyLee05 <NanJun05@163.com>
2017-08-28 10:26:51 +0800

    add new files

---
+++ SwiftLearn/MTRadioTableViewCell.py
@@ -0,0 +1,11 @@
+import UIKit
+
+class MTRadioTableViewCell: MTBaseTableViewCell {
+	
+	@IBOutlet weak var radioButton: UIImageView!
+	@IBOutlet weak var nameLabel: UILabel!
+
+	override func awakeFromNib() {
+		super.awakeFromNib()
+	}
+}
--- SwiftLearn/MYSliderTableViewCell.py
+++ SwiftLearn/MYSliderTableViewCell.py
@@ -1,1 +1,26 @@
 import UIKit
+
+class MTSliderTableViewCell: MTBaseTableViewCell {
+	
+	@IBOutlet weak var descLabel: UILabel!
+	@IBOutlet weak var sliderView: SliderView!
+
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		descLabel.text = NSLocalizedString("LOST_SLIDER_TIPS", comment: "")
+
+		let panGestureRecognier = UIPanGestureRecognizer(target: self, action: #selector
+			(panGestureRecognierHandler(GestureRecognier:)))
+		panGestureRecognier.delegate = self
+		addGestureRecognizer(panGestureRecognier)
+	}
+
+	func panGestureRecognierHandler(GestureRecognier: UIPanGestureRecognizer) {
+
+	}
+
+	override func GestureRecognier(_ gestureRecognier: UIGestureRecognizer,
+								  shouldRecognizerSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+		return false
+	}
+}
