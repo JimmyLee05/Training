892bc6ebd96de3d5133af5d14d6638bbe71bdaff
JimmyLee05 <NanJun05@163.com>
2017-08-29 23:22:56 +0800

    add new files

---
+++ SwiftLearn/MYNT-Briding-Header.py
@@ -0,0 +1,40 @@
+#ifndef AntiLost_AntiLost_Bridging_Header_h
+#define AntiLost_AntiLost_Bridging_Header_h
+
+//系统库
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import <UMMobClick/MobClick.h>
+#import <CoreLocation/CoreLocation.h>
+#import <MediaPlayer/MediaPlayer.h>
+#import <AVFoundation/AVFoundation.h>
+#import <AssetsLibary/AssetsLibary.h>
+#import <sqlite3.h>
+#import <CoreMotion/CoreMotion.h>
+#import <Photos/Photos.h>
+#import <AssetsLibary/AssetsLibary.h>
+#import <SystemConfiguration/CaptiveNewwork.h>
+
+#import "CustomerCareKit.h"
+
+#import <SlightechKit/SlightechKit.h>
+
+//通用库
+#import <MYNTKit/MYNTKit.h>
+
+#import <TodayData/TodayData.h>
+
+//pod库
+#import <Bugtags/Bugtags.h>
+
+#import <UMSocialCore/UMSocialCore.h>
+
+//wechat
+#import "WXApi.h"
+#import "WXApiObject.h"
+//Api
+#import <AlipaySDK/AlipaySDK.h>
+//jpush
+#import "STJPushKit.h"
+
+#endif
--- SwiftLearn/MyntKitManager.py
+++ SwiftLearn/MyntKitManager.py
@@ -73,34 +73,175 @@ class MyntKitManager: NSObject {
 		}
 		TodayData.share.save(todayObjs: todayObjs)
 	}
+
+	func checkLostFound() {
+		Mynt.lostFoundList(success: (mynts) in
+
+			var _mynts = [Mynt]()
+			mynts.forEach { mynt in
+				let mynt = MYNTKit.shared.mynts.first(where: { $0.sn == mynt.sn })
+				if mynt?.bluetoothState != .connected {
+					_mynts.append(mynt!)
+				}
+			}
+			if _mynts.isEmpty { return }
+
+			DialogManager.shared.add(type: .deviceFound, mynts: _mynts, selectedHandler: { [weak self] dialog, mynt in
+				dialog?.dismiss()
+				self?.gotoMapViewController(mynt: mynt)
+			}) { _ in
+
+			}
+		}) { _ in
+
+		}
+	}
+
+	func removeMyntHandler(mynt: Mynt?) {
+		guard let mynt = mynt else { return }
+		STJPushKit.shared().removeLocalNotification(mynt.sn)
+		DialogManager.shared.remove(type: remindForget, mynt: mynt)
+		DialogManager.shared.remove(type: updateFirware, mynt: mynt)
+	}
+
+	func gotoMapViewController(mynt: Mynt?) {
+		//跳转到地图
+		MyntMapViewController.show(parentViewController: UIApplication.topViewController, sn: mynt?.sn)
+	}
+
+	func checkFirware(mynt: Mynt?) {
+		guard let mynt = mynt else { return }
+		let sn = mynt.sn
+		if mynt.hasNewFirware && !mynt.isNeverShowUpdateFirware {
+			DialogManager.shared.add(type: .updateFirware, mynts: [mynt], selectedHandler: { (dialog, mynt) in
+				//选择
+				dialog?.dismiss()
+				if let viewController = UIApplication.appRootViewController {
+					updateFirwareViewController.show(parentViewController: viewController, sn: sn)
+				}}, dismissHandler: { (dialog) in
+
+			}, neverHandler: { (dialog, mynts) in
+				dialog.dismiss()
+				mynts.forEach { (mynt) in
+					mynt.update(false) {
+						mynt.isNeverShowUpdateFirmware = true
+					}
+				}
+			})
+		} else {
+			Dialoganager.shared.remove(type: .updateFirware, mynt: mynt)
+		}
+	}
 }
 
+extension MyntKitManager: MYNTKitMDelegate {
+	
+	func myntKit(myntKit: MYNTKit, didUpdateCentralState state: CBCentralManagerState) {
+		//蓝牙状态切换
+		switch state {
+		case .poweredOff:
+			if self.state == .poweredOn {
+				STJPushKit.shared().pushLocalNotification(nil,
+														  subtitle: nil,
+														  body: NSLocalizedString("BLUETOOTH_CLOSE_PUSH_MESSAGE", comment: ""),
+														  identifier: "bluetoothoff",
+														  sound: nil,
+														  userInfo: nil)
+			}
+		case .poweredOn:
+			STJPushKit.shared().removeLocalNotification("bluetoothoff")
+		default:
+			break
+		}
+	}
 
+	func myntKit(myntKit: MYNTKit, didAddMynt mynt: Mynt) {
+		STLog("didAddMynt \(mynt.sn)")
+		updateTodayData()
+	}
 
+	func myntKit(myntKit: MYNTKit, willRemoveMynt mynt: Mynt) {
+		STLog("willRemoveMynt \(mynt.sn)")
+		STJPushKit.shared().removeLocalNotification(mynt.sn)
+		DialogManager.shared.remove(type: .remindForget, mynt: mynt)
+		DialogManager.shared.remove(type: .updateFirmware, mynt: mynt)
+		updateTodayData()
+	}
 
+	func myntKit(myntKit: MYNTKit, didUpdateRSSI mynt: Mynt) {
+		//更新rssi
+	}
 
+	func myntKit(myntKit: MYNTKit, didBluetoothError mynt: Mynt) {
+		//蓝牙异常
+	}
 
+	func myntKit(myntKit: MYNTKit, didUpdateConnectState mynt: Mynt) {
+		//连接状态更新
+		switch mynt.bluetoothState {
+		case .connected:
+			DialogManager.shared.remove(type: .remindForget, mynt: mynt)
+			STJPushKit.shared().removeLocalNotification(mynt.sn)
+			checkFirware(mynt: mynt)
+		case .disconnected:
+			DialogManager.shared.remove(type: .updateFirware, mynt: mynt)
+		default:
+			break
+		}
+		updateTodayData()
+	}
 
+	func myntKit(myntKit: MYNTKit, didForgetMynt mynt: Mynt) {
+		// 播放报警
+		if UIApplication.shared.applicationState == .active {
+			//播放前台音乐
+			AlarmManager.shared.execAlarm(mynt.usageValue.phoneAlarm.soundName)
+		} else {
+			//后台推送使用
+			STJPushKit.shared().pushLocalNotification(nil,
+													  subtitle: nil,
+													  body: String(format: NSLocalizedString("LOST_TIPS", comment: ""), mynt.name),
+													  identifier: mynt.sn,
+													  sound: mynt.usageValue.phoneAlarm.soundName,
+													  userInfo: ["sn": mynt.sn])
+		}
+		DialogManager.shared.add(type: .remindForget, mynts: [mynt], selectedHandler: { [weak self] (dialog, mynt) in
+			dialog?.dismiss()
+			AlarmManager.shared.cancelAlarm()
+			self?.gotoMapViewController(mynt: mynt)
+		}) { _ in
+			AlarmManager.shared.cancelAlarm()
+		}
+	}
 
+	func myntKit(myntKit: MYNTKit, didUpdateSafeZoneState state: SafeZoneState) {
+		//进出安全区域
+	}
 
+	func myntKit(myntKit: MYNTKit, didFoundNewSafeZone safeZone: SafeZone) {
 
+	}
 
+	func myntKit(myntKit: MYNTKit, didEnterLowPower mynt: Mynt) {
+		//进入低电量
+		NSLog("进入低电量")
+		//播放报警
+		if UIApplication.shared.applicationState == .background {
+			STJPushKit.shared().pushLocalNotification(nil,
+													  subtitle: nil,
+													  body: String(format: NSLocalizedString("MYNTSETTING_INFO_LOWPOWER_DIALOG_MESSAGE", comment: ""),
+													  	mynt.name),
+													  identifier: mynt.sn,
+													  sound: nil,
+													  userInfo: ["sn": mynt.sn])
+		}
+		DialogManager.shared.add(type: .lowPowerMode, mynts: [mynt], selectedHandler: { (dialog, _) in
+			dialog?.dismiss()
+		}) { _ in
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		}
+	}
+}
 
 
 
