34674375c93f1775aba0c1b932513b9a60edcb6a
JimmyLee05 <NanJun05@163.com>
2017-09-02 14:35:23 +0800

    add new files

--- SwiftLearn/STCentralManager.py
+++
@@ -1,9 +0,0 @@
-#import "YMSCBCentralManager.h"
-#import "STPeripheralManager.h"
-
-@class STPeripheral, STCentralManager;
-@protocol STCentralManagerDelegate <NSObject>
-
-@optional
-
-- (void)centralManager:()
