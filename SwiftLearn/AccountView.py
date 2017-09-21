6a17111ffa2d21d01aa01ccb05eae40f6c5834db
JimmyLee05 <NanJun05@163.com>
2017-09-21 10:31:18 +0800

    add new files

--- SwiftLearn/AccountView.py
+++ SwiftLearn/AccountView.py
@@ -64,6 +64,102 @@ class AccountView: UIView, UIActionSheetDelegate {
 		logoutButton.setTitle(MTLocalizedString("LOGOUT", comment: "注销"), for: UIControlState.normal)
 	}
 
+	/**
+	弹出框回调，预留
+
+	- parameter actionSheet: actionSheet description
+	- parameter buttonIndex: buttonIndex description
+	*/
+
+	func actionSheet(_ actionSheet: UIActionSheet, clickedButtonAt buttonIndex: Int) {
+		// 0 -> 取消 1开始计算
+		STLog("clickedButtonAtIndex \(buttonIndex)")
+	}
+
+	/**
+	初始化信息
+	*/
+	fileprivate func _initInfoView() {
+		/* 添加箭头锚点 */
+		arrowImageView.layer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+
+		/* 初始化手势 */
+		let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didClickInfoView))
+		infoView.addGestureRecognizer(tapGestureRecognizer)
+	}
+
+	/**
+	初始化编辑
+	*/
+	fileprivate func _initEditView() {
+		editViewHeight 					= editView.bounds.height
+		editHeightConstraint.constant 	= 0
+
+		editView.backgroundColor 		= expandBackgroundColor
+		// 添加手势
+		albumView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didClickEditThumb(tapGestureRecognizer:))))
+		pictureView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didClickedEditThumb(tapGestureRecognizer:))))
+		defaultThumbView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didClickEditThumb(tapGestureRecognizer:))))
+	}
+
+	/**
+	初始化button
+	*/
+	fileprivate func initButtons() {
+		changePasswordButton.setButtonBackgroundColorStyle(ColorStyle.kTunaGradientColor)
+		logoutButton.setButtonBackgroundColorStyle(ColorStyle.kRedGradientColor)
+	}
+
+	/**
+	加载信息
+	*/
+	func updateInfo() {
+		nameLabel.text 			= MYNTKit.shared.user?.userName
+		myntCountLabel.text 	= String(format: MTLocalizedString("MYNTS_IN_TOTAL", comment: "你有n个小觅"), MYNTKit.shared.mynts.count)
+		avatarImageView.image 	= MYNTKit.shared.user?.avatar
+	}
+
+	func updateMyntCount() {
+		myntCountLabel.text 	= String(format: MTLocalizedString("MYNTS_IN_TOTAL", comment: "你有n个小觅")， MYNTKit.shared.mynts.count)
+	}
+
+	func didClickEditThumb(tapGestureRecognizer: UITapGestureRecognizer) {
+		guard let view = tapGestureRecognizer.view else {
+			return
+		}
+		guard let viewController = viewController else {
+			return
+		}
+		switch view {
+		case albumView:
+			SelectImageUtils.shared.selectImageFromPhotos(viewController: viewController, edit: true) { image in
+				MYNTKit.shared.user?.changeAvatar(avatar: image, success: {
+
+				}) { _, msg in
+					MTToast.show(msg)
+
+				}
+			}
+		case pictureView:
+			SelectImageUtils.shared.selectImageFromCamera(viewController: viewController, edit: true) { image in
+				MYNTKit.shared.user?.changeAvatar(avatar: image, success: {
+
+				}) { _, msg in
+					MTToast.show(msg)
+				}
+			}
+		case defaultThumbView:
+			MYNTKit.shared.user?.deleteAvatar(success: {
+
+			}) { _, msg in
+				MTToast.show(msg)
+			}
+		default:
+			break
+
+		}
+	}
+
 	
 
 	}
