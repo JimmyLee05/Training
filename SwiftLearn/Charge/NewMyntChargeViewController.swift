ce16b4ec68e6c8de3814cdd73c9acf662b68103f
JimmyLee05 <NanJun05@163.com>
2017-09-29 17:35:14 +0800

    add new files

---
+++ SwiftLearn/Charge/NewMyntChargeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewMyntChargeViewController.swift
+//  MYNT
+//
+//  Created by 李南君 on 2017/9/29.
+//  Copyright © 2017年 slightech. All rights reserved.
+//
+
+import UIKit
+
+class NewMyntChargeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
