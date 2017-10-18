9b2fc6df81b780686e5165ac3e99979db1102227
JimmyLee05 <NanJun05@163.com>
2017-10-18 14:48:26 +0800

    add OAD file

---
+++ SwiftLearn/NewShareViewController.swift
@@ -0,0 +1,45 @@
+//
+//  NewShareViewController.swift
+//  MYNT
+//
+//  Created by 李南君 on 2017/10/18.
+//  Copyright © 2017年 slightech. All rights reserved.
+//
+
+import UIKit
+import Slightech
+
+class ShareViewController: BaseViewController {
+    
+    class func present(parentViewController: UIViewController?, mynt: Mynt?) {
+        
+    }
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override var isShowBackgroundLayer: Bool { return false }
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    override func leftBarButtonCilckedHandler() {
+        
+    }
+    
+    @IBAction func didClickLinkButton() {
+        
+    }
+    
+    @IBAction func didClickWechatButton() {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+    
+    }
+    
+    func gotoAskPreviewViewController() {
+        
+    }
+}
