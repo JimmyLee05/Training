3c42f8c0b581f0d831fce17314654b49509527da
JimmyLee05 <NanJun05@163.com>
2017-10-20 14:14:39 +0800

    add new file

---
+++ SwiftLearn/NewMyntInfoControlExpandSubView.swift
@@ -0,0 +1,19 @@
+//
+//  NewMyntInfoControlExpandSubView.swift
+//  MYNT
+//
+//  Created by 李南君 on 2017/10/20.
+//  Copyright © 2017年 slightech. All rights reserved.
+//
+
+import UIKit
+
+class MyntInfoControlExpandSubView: MyntInfoBaseSubView {
+    
+    class ClickEventView: UIView {
+        
+        var clickEvent: MYNTClickEvent = .click {
+            
+        }
+    }
+}
