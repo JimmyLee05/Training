577386b7551fb7f40039d0826ca2b97b7b435448
JimmyLee05 <NanJun05@163.com>
2017-10-23 09:34:46 +0800

    add new files

---
+++ SwiftLearn/NewMyntInfoLossSubView.swift
@@ -0,0 +1,14 @@
+//
+//  NewMyntInfoLossSubView.swift
+//  MYNT
+//
+//  Created by 李南君 on 2017/10/23.
+//  Copyright © 2017年 slightech. All rights reserved.
+//
+
+import UIKit
+
+class MyntInfoLossSubView: MyntInfoBaseSubView {
+    
+    
+}
