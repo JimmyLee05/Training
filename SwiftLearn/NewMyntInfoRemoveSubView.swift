a91aa76affad59ac27a891128bb52fd41ca57aec
JimmyLee05 <NanJun05@163.com>
2017-10-26 15:46:48 +0800

    add new files

---
+++ SwiftLearn/NewMyntInfoRemoveSubView.swift
@@ -0,0 +1,14 @@
+//
+//  NewMyntInfoRemoveSubView.swift
+//  MYNT
+//
+//  Created by 李南君 on 2017/10/26.
+//  Copyright © 2017年 slightech. All rights reserved.
+//
+
+import UIKit
+
+class MyntInfoRemoveSubView: MyntInfoBaseSubView {
+    
+    
+}
