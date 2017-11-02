efd31a9562d8633e5522f9aec7727dd8658c7b3f
JimmyLee05 <NanJun05@163.com>
2017-11-02 16:27:35 +0800

    add new files

---
+++ SwiftLearn/NewSearchTipsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewSearchTipsViewController.swift
+//  MYNT
+//
+//  Created by 李南君 on 2017/11/2.
+//  Copyright © 2017年 slightech. All rights reserved.
+//
+
+import UIKit
+import MYNTKit
+import Lottie
+
+class NewSearchTipsViewController: UIViewController {
+    
+    var animation: String {
+        
+    }
+}
