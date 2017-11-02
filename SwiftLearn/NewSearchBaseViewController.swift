65f861b785bf1ff4ea113eb0da39b2e6628f3ab8
JimmyLee05 <NanJun05@163.com>
2017-11-02 15:30:06 +0800

    add new files

---
+++ SwiftLearn/NewSearchBaseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewSearchBaseViewController.swift
+//  MYNT
+//
+//  Created by 李南君 on 2017/11/2.
+//  Copyright © 2017年 slightech. All rights reserved.
+//
+
+import UIKit
+import MYNTKit
+
+private let kCloseNotification = NSNotification.Name("kClosedNotification")
+
+class SearchBaseViewController: BaseViewController {
+    
+    
+}
